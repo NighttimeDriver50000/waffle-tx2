94dcf10cd2b2aeaa527e380fa93ea02dd1ad842d
Chris McKinney and Sam Dauchert <maestro@tachibanatech.com>
2019-04-16 15:57:53 -0400

    Launch files, object for odom node, no-op nodes

--- waffle_tx2_tf2/src/tf2_odom_node.cpp
+++ waffle_tx2_tf2/src/tf2_odom_node.cpp
@@ -5,32 +5,43 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/TransformStamped.h>
 
-tf2_ros::TransformBroadcaster caster;
-tf2::Transform current;
-
-void correctionCallback(const geometry_msgs::TransformStamped::ConstPtr& msg)
+class OdomCaster
 {
-  tf2::Transform offset;
-  tf2::fromMsg(msg->transform, offset);
-  current *= offset;
-  // Stamp and send the transform.
-  geometry_msgs::TransformStamped stamped;
-  stamped.header.stamp = msg->header.stamp;
-  stamped.header.frame_id = "map";
-  stamped.child_frame_id = "odom";
-  stamped.transform = tf2::toMsg(current);
-  caster.sendTransform(stamped);
-}
+  public:
+    OdomCaster()
+    {
+      tf2::Quaternion q;
+      q.setRPY(0, 0, 0);
+      current_ = tf2::Transform();
+      sub_ = node_.subscribe("odom_corrections", 16,
+          &OdomCaster::correctionCallback, this);
+    }
+
+    void correctionCallback(const geometry_msgs::TransformStamped::ConstPtr& msg)
+    {
+      tf2::Transform offset;
+      tf2::fromMsg(msg->transform, offset);
+      current_ *= offset;
+      // Stamp and send the transform.
+      geometry_msgs::TransformStamped stamped;
+      stamped.header.stamp = msg->header.stamp;
+      stamped.header.frame_id = "map";
+      stamped.child_frame_id = "odom";
+      stamped.transform = tf2::toMsg(current_);
+      caster_.sendTransform(stamped);
+    }
+  
+  private:
+    tf2_ros::TransformBroadcaster caster_;
+    tf2::Transform current_;
+    ros::NodeHandle node_;
+    ros::Subscriber sub_;
+};
 
 int main(int argc, char** argv)
 {
-  tf2::Quaternion q;
-  q.setRPY(0, 0, 0);
-  current = tf2::Transform();
   ros::init(argc, argv, "tf2_odom_node");
-  ros::NodeHandle node("waffle_tx2");
-  ros::Subscriber sub = node.subscribe("odom_corrections", 16,
-      &correctionCallback);
+  OdomCaster caster;
   ros::spin();
   return 0;
 }
--- waffle_tx2_tf2/src/tf2_zed_node.cpp
+++ waffle_tx2_tf2/src/tf2_zed_node.cpp
@@ -1,4 +1,8 @@
+#include <ros/ros.h>
+
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "tf2_zed_node");
+  ros::NodeHandle node;
   return 0;
 }
